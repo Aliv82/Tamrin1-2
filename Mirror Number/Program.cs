ceedce5d74cc8269f6146c197e8eeaaafa79dbf2
Ali.v_82 <alivah1382@gmail.com>
2023-10-06 23:13:18 +0330

    khoooobe!!

---
+++ Mirror Number/Program.cs
@@ -0,0 +1,22 @@
+﻿//Ali Vahedi
+//Mirror_Number
+Console.WriteLine("please insert your first number : ");
+int firstnumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("please insert your second number : ");
+int secondnumber = int.Parse(Console.ReadLine());
+
+for (int i = firstnumber; i <= secondnumber; i++)
+{
+    if (i.ToString().Equals(Reverse(i.ToString())) && 11 <= i)
+        Console.WriteLine(i);
+
+}
+Console.ReadLine();
+
+static string Reverse(string s)
+{
+    char[] charArray = s.ToCharArray();
+    Array.Reverse(charArray);
+    return new string(charArray);
+}
